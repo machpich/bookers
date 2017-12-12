754bf5e630716f566a711a77d6636243d7fc17da
shkobm <scrt.8shbabe@gmail.com>
2017-12-12 18:49:57 +0900

    3-8 framework completed, still not CSS&HTML

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def new
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	@book.save
+  	redirect_to book_path(@book)
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	redirect_to new_book_path
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to new_book_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
